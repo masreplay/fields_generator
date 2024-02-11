21c325a299010bacea1152fb2c4e81927cdc8f34
M Atheer <55885230+masreplay@users.noreply.github.com>
2024-02-11 20:03:04 +0300

    Add fields_generator package and update dependencies

--- example/lib/library_source.dart
+++ example/lib/library_source.dart
@@ -1,10 +1,6 @@
-// Copyright (c) 2018, the Dart project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'dart:math';
 
-import 'package:source_gen_example/annotations.dart';
+import 'package:fields_generator/annotations.dart';
 
 part 'library_source.g.dart';
 
--- generator/README.md
+++
@@ -1,3 +0,0 @@
-An Dart package demonstrating how to create `source_gen`-based builders. 
-
-See also the `example_usage` directory.
